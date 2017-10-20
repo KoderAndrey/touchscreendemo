65afe5d30e188d2a242693f3ebc287bca62426f3
Unknown <usevalad.uladzimiravich@gmail.com>
2017-10-20 16:47:40 +0300

    views added

--- app/src/main/java/com/freedroider/touchscreendemo/ui/activity/ClientActivity.java
+++ app/src/main/java/com/freedroider/touchscreendemo/ui/activity/ClientActivity.java
@@ -4,46 +4,28 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.freedroider.touchscreendemo.R;
 import com.freedroider.touchscreendemo.constant.Constant;
+import com.freedroider.touchscreendemo.model.ControlAction;
+import com.freedroider.touchscreendemo.model.Ratio;
 import com.freedroider.touchscreendemo.service.MyService;
-import com.freedroider.touchscreendemo.udp.UDPHelper;
-import com.freedroider.touchscreendemo.ui.activity.view.TVCursorView;
+import com.freedroider.touchscreendemo.ui.activity.view.RemoteControlView;
 import com.freedroider.touchscreendemo.utils.Logger;
-import com.google.gson.Gson;
-
-import java.io.IOException;
 
 import butterknife.BindView;
-import butterknife.OnClick;
-import butterknife.Optional;
 
-public class ClientActivity extends BaseActivity {
+public class ClientActivity extends BaseActivity implements RemoteControlView.OnRemoteControlListener {
     @BindView(R.id.viewRemoteControl)
-    TVCursorView tvCursorView;
+    RemoteControlView remoteControlView;
     Intent intent;
-
-//    @OnClick(R.id.send_button)
-//    void onClick() {
-//        Logger.d(TAG, "onClick: ");
-//        String message = editText.getText().toString();
-//        String json = getApp().getGSON().toJson(message);
-//        Intent broadcast = new Intent(MyService.ACTION_SEND_DATA);
-//        broadcast.putExtra(Constant.INTENT_KEY_JSON, json);
-//        sendBroadcast(broadcast);
-//        Logger.d(TAG, "onClick: send boradcast. message = " + message);
-//    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         intent = new Intent(this, MyService.class);
         startService(intent);
+        remoteControlView.setControlListener(this);
     }
 
     @Override
@@ -60,5 +42,25 @@ public class ClientActivity extends BaseActivity {
     public static void start(Context context) {
         Intent starter = new Intent(context, ClientActivity.class);
         context.startActivity(starter);
+    }
+
+    @Override
+    public void onSizeReady(int width, int height) {
+        Logger.d("onSizeReady");
+        String json = getApp().getGSON().toJson(new Ratio(width, height));
+        Intent broadcast = new Intent(MyService.ACTION_SEND_DATA);
+        broadcast.putExtra(Constant.INTENT_KEY_JSON, json);
+        sendBroadcast(broadcast);
+        Logger.d("onClick: send boradcast. message = " + json);
+    }
+
+    @Override
+    public void onMove(float angle, double distance) {
+        Logger.d("onMove");
+        String json = getApp().getGSON().toJson(new ControlAction(angle, distance));
+        Intent broadcast = new Intent(MyService.ACTION_SEND_DATA);
+        broadcast.putExtra(Constant.INTENT_KEY_JSON, json);
+        sendBroadcast(broadcast);
+        Logger.d("onClick: send boradcast. message = " + json);
     }
 }
